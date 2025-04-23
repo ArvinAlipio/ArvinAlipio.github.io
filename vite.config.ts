d095d573f565276844fc7745d32fe055141864bf
Aj <arvinalipio@gmail.com>
2025-04-24 05:54:37 +0800

    updated vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/future-tech-persona/' : '/',
+  // base: mode === 'production' ? '/future-tech-persona/' : '/',
+  base:  "/ArvinAlipio.github.io/",
   server: {
     host: "::",
     port: 8080,
